0eb11f0b2e30495d825a5a0ecdc1e7fec9cf2366
Cyrille Rossant <rossant@users.noreply.github.com>
2024-03-09 21:56:22 +0100

    Improved color palette

--- scripts/constants.js
+++ scripts/constants.js
@@ -14,11 +14,35 @@ grid = js.window.app.grid
 
 ${locale.black} = (0, 0, 0)
 ${locale.white} = (255, 255, 255)
-${locale.red} = (255, 0, 0)
-${locale.green} = (0, 255, 0)
-${locale.blue} = (0, 0, 255)
-${locale.yellow} = (255, 255, 0)
-${locale.orange} = (255, 100, 0)
+
+# ${locale.red} = (255, 0, 0)
+# ${locale.green} = (0, 255, 0)
+# ${locale.blue} = (0, 0, 255)
+# ${locale.yellow} = (255, 255, 0)
+# ${locale.orange} = (255, 100, 0)
+
+# Basic and Softened Colors
+${locale.red} = (230, 90, 90);
+${locale.green} = (80, 200, 120);
+${locale.blue} = (100, 150, 240);
+${locale.yellow} = (255, 230, 100);
+${locale.orange} = (255, 170, 70);
+
+# Additional Colors for Variety
+${locale.purple} = (150, 100, 220);
+${locale.teal} = (70, 200, 200);
+${locale.pink} = (255, 150, 180);
+${locale.peach} = (255, 180, 150);
+${locale.limeGreen} = (180, 240, 100);
+${locale.skyBlue} = (135, 206, 235);
+${locale.lavender} = (200, 150, 220);
+${locale.mint} = (170, 255, 195);
+${locale.navy} = (0, 70, 130);
+${locale.maroon} = (128, 50, 50);
+${locale.olive} = (128, 128, 0);
+${locale.coral} = (255, 127, 80);
+${locale.aqua} = (127, 255, 212);
+
 
 def ${locale.size}(n, m):
     grid.reshape(n, m)
